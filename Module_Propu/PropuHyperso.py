0185e01e2d8265f46cd4aa1a396a8ea1f593a263
AlexandreRubenstrunk <alexandre.rubenstrunk@estaca.eu>
2024-11-07 14:24:47 +0100

    Formule de base propulsion récacteur

--- Module_Propu/PropuHyperso.py
+++ Module_Propu/PropuHyperso.py
@@ -1,2 +1,11 @@
 import numpy as np
 
+class Engine():
+
+    def __init__(self):
+        self.A_exit=float()
+
+def Thrust(Engine_Studied:Engine,debit_m_inlet, Speed_inlet, P_inlet):
+    """this function aime to calculte the thrust generate by the studied reactor"""
+
+    F = debit_m_exit * Speed_exit - debit_m_inlet * Speed_inlet + (P_exit-P_inlet) * Engine_Studied.A_exit
